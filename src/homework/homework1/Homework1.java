23ea436d5a0f207eb833a99032f8c3154aec6648
riddle671 <riddle671@yandex.ru>
2023-11-24 15:38:39 +0300

    24.11.23-3

--- src/homework/homework1/Homework1.java
+++ src/homework/homework1/Homework1.java
@@ -44,10 +44,10 @@ package homework.homework1;
             // когда топлива не меньше 10 литров, двигатель работает, колеса все работают, нет ошибок
             //В ином случае, машина не должна запускаться
             if (
-                    fuel < 10
-                            ||  (!isWheelWork1 || isWheelWork2 || isWheelWork3 || isWheelWork4)
-                            && hasErrors
-                            || isEngineWork
+                    fuel >= 10
+                            &&  (isWheelWork1 && isWheelWork2 && isWheelWork3 && isWheelWork4)
+                            && (!hasErrors)
+                            && isEngineWork
             ) {
                 System.out.println("Машина работает");
             } else {
