83716c2f2399315fb200d2d9f8714285eb7ff54b
riddle671 <riddle671@yandex.ru>
2023-12-06 19:54:08 +0300

    06.12.23-4

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -2,16 +2,6 @@ package shop;
 
 public class Shop {
 
-    Worker name;
-    Worker age;
-
-    public Shop(Worker name, Worker age) {
-
-        this.name = name;
-        this.age = age;
-
-    }
-}
 
 
 
