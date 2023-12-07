8ed555c9feef494213f213a4493ef3dba177746d
riddle671@yandex.ru <12345Xxx2>
2023-12-07 22:15:09 +0300

    07.12.23-8

--- src/homework/homework2/shop/Runner.java
+++ src/homework/homework2/shop/Runner.java
@@ -6,13 +6,13 @@ public class Runner {
         Item Screwdriver = new Item("отвертка", 50);
         Item Notebook = new Item("тетрадка",30);
         Item Pen = new Item("ручка", 10);
-        Item[] setThings1 = {Hammer, Screwdriver};
-        Item[] setThings2 = {Notebook, Pen};
-        Worker worker1 = new Worker("Василий", 30, "мужчина", setThings1 );
-        Worker worker2 = new Worker("Марьяна", 25, "женщина", setThings2 );
+        Item[] setThingsVasilij = {Hammer, Screwdriver};
+        Item[] setThingsMarjana = {Notebook, Pen};
+        Worker worker1 = new Worker("Василий", 30, "мужчина", setThingsVasilij );
+        Worker worker2 = new Worker("Марьяна", 25, "женщина", setThingsMarjana );
         Worker[] workmax = {worker1, worker2};
         Shop workShop = new Shop(workmax);
-        worker1.boasting(worker1.name, setThings2);
-        worker2.boasting(worker2.name, setThings1);
+        worker1.boasting(worker1.name, setThingsMarjana);
+        worker2.boasting(worker2.name, setThingsVasilij);
     }
 }
