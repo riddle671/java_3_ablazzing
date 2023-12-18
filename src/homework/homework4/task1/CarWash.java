50ad353f8979d8c2ea7bc74a8eab0ddc335d5d98
riddle671@yandex.ru <12345Xxx2>
2023-12-18 23:17:24 +0300

    18.12.23-6

--- src/homework/homework4/task1/CarWash.java
+++ src/homework/homework4/task1/CarWash.java
@@ -2,41 +2,32 @@ package homework.homework4.task1;
 
 public class CarWash {
 
+    int costWash;
+    int taxAuto = 2000;
+    int taxBus = 4000;
+
+    public int getCostWash() {
+        return costWash;
+    }
+
     public int moika(Car  car) {
         car.setChist(true);
         if (car.getLength() > 6 || car.getHeight() > 2.5 || car.getWidth() > 2) {
-            return 4000;
+            return costWash += taxBus;
         } else {
-            return 2000;
+            return costWash += taxAuto;
         }
     }
 
     public int moika2(Car[]  car) {
-        int sum = 0;
-        int taxAuto = 4000;
-        int taxBus = 2000;
         for (Car cars : car) {
             if (cars.getLength() > 6 || cars.getHeight() > 2.5 || cars.getWidth() > 2) {
-                return sum += taxBus;
+                return costWash += taxBus;
             } else {
-                return sum += taxAuto;
+                return costWash += taxAuto;
             }
         }
-        return sum;
+        return costWash;
     }
 
-
-    public static void main(String[] args) {
-        CarWash carWash1 = new CarWash();
-        Car car = new Bus(false, 7, 1, 1, 20);
-        Car car2 = new Bus(false, 8, 1, 1, 20);
-        Car car3 = new Bus(false, 8, 1, 1, 20);
-        Car[] cars = new Car[] {car, car2, car3};
-        //carmoik1.moika(car);
-        System.out.println(carWash1.moika(car));
-        //System.out.println(car.chist);
-        //carmoik1.moika2(rrr);
-        //System.out.println(carWash1.moika2(cars));
-
-    }
 }
--- src/homework/homework4/task1/Runner.java
+++ src/homework/homework4/task1/Runner.java
@@ -2,6 +2,19 @@ package homework.homework4.task1;
 
 public class Runner {
     public static void main(String[] args) {
+        CarWash carWash1 = new CarWash();
+        Car car = new Bus(false, 1, 1, 1, 20);
+        Car car2 = new Bus(false, 8, 1, 1, 20);
+        Car car3 = new Bus(false, 8, 1, 1, 20);
+        Car[] cars = new Car[] {car, car2, car3};
+        carWash1.moika(car);
+        System.out.println(carWash1.getCostWash());
+        carWash1.moika2(cars);
+        System.out.println(carWash1.getCostWash());
+
+
+
+
 
     }
 }
